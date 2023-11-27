da9c2cf3ae1889197d4f0f610d71c31e8d68fe90
Yonathan Shtekel <yonathan.shtekel@riskified.com>
2023-11-27 21:27:24 +0200

    UI Enhancements

--- whatsapp-chrome-extensions/index.js
+++ whatsapp-chrome-extensions/index.js
@@ -1,27 +1,48 @@
 const actionToRepresentation = {
-  goToShalterRoket: {
-    icon: "1",
-    hebrew: "טילים",
-    russian: "ракеты",
-    thai: "ขีปนาวุธ",
+  goToShelter: {
+    icon: "🚨",
+    hebrew: "חירום - להיכנס לממד",
+    description: "נא להיכנס למרחבים מוגנים ולסגור דלתות בתים.\n" +
+        "נא להמשיך להישמע להנחיות פיקוד העורף\n",
+    russian: "Чрезвычайная ситуация - введите измерение",
+    thai: "ฉุกเฉิน - เข้ามิติ",
   },
-  terroristInfiltration: {
-    icon: "2",
-    hebrew: "חדירת מחבלים",
-    russian: "террористическое проникновение",
-    thai: "การแทรกซึมของผู้ก่อการร้าย",
+  stayInShelter: {
+    icon: "🚨",
+    hebrew: "חירום - להישאר בממד",
+    description: "יש להמשיך לשהות בממד עד להודעה חדשה.\n" +
+        "אין לצאת מהממד ללא הודעה מפורשת של צח\"י\n",
+    russian: "Чрезвычайная ситуация - оставайтесь в измерении",
+    thai: "ช่างซ่อมนาฬิกา-ดูออนไลน์",
   },
-  goToShalterAntiTank: {
-    icon: "3",
-    hebrew: "נגד טנקים",
-    russian: "противотанковый",
-    thai: "ต่อต้านรถถัง",
+  stayNearByShelter: {
+    icon: "📢",
+    hebrew: "להיות בקרבת ממד",
+    description: "ניתן לצאת מן הממ\"דים.\n" +
+        "נבקש לעת עתה להישאר בקרבת מרחב מוגן\n",
+    russian: "быть в непосредственной близости",
+    thai: "ที่จะอยู่ใกล้กัน",
   },
-  hostileAircraft: {
-    icon: "4",
-    hebrew: "כלי טיס עוין",
-    russian: "rвражеский самолетr7",
-    thai: "เครื่องบินที่ไม่เป็นมิตร",
+  ShelterOut: {
+    icon: "🆓",
+    hebrew: "ניתן לצאת מהממד",
+    description: "להישאר בקרבת מרחב מוגן",
+    russian: "Вы можете выйти из измерения",
+    thai: "คุณสามารถออกจากมิติได้",
+  },
+  friendlyFire: {
+    icon: "🪖",
+    hebrew: "קולות ירי של כוחותינו",
+    description: "קולות הירי הנשמעים כעת הם כתוצאה מירי של צה\"ל. הישוב בשגרה.\n",
+    russian: "Выстрелы наших сил",
+    thai: "เสียงปืนของกองกำลังของเรา",
+  },
+  freeText: {
+    icon: "❗",
+    hebrew: "טקסט חופשי",
+    description: "הדבק טקסט חופשי כאן",
+    russian: "обращать внимание",
+    thai: "ใส่ใจ",
   },
 };
 
@@ -35,6 +56,29 @@ document
     navigator.clipboard.writeText(await genMessage(actionInfo, givenMessage));
     document.getElementById("messageStatus").innerHTML = "ההודעה נוצרה";
   });
+
+// on change of select, update the text area
+document.getElementById("neededAction").addEventListener("change", function () {
+    const neededAction = document.getElementById("neededAction").value;
+    const actionInfo = actionToRepresentation[neededAction];
+    document.getElementById("givenMessage").value = actionInfo.description;
+});
+
+Object.keys(actionToRepresentation).forEach((action, index) => {
+  const option = document.createElement("option");
+  const icon = actionToRepresentation[action].icon;
+  const hebrew = actionToRepresentation[action].hebrew;
+  const description = actionToRepresentation[action].description;
+
+  option.value = action;
+  option.innerHTML = hebrew + " " + icon;
+  document.getElementById("neededAction").appendChild(option);
+
+  // Initialize the textarea with the description message only for the first option
+  if (index === 0) {
+    document.getElementById("givenMessage").value = description;
+  }
+});
 
 async function genMessage(actionInfo, givenMessage) {
   return `
